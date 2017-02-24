c8cb352572483120a3f710728906d553b209e8cf
JugglerX <rob@jugglerdigital.com>
2017-02-23 20:54:57 -0800

    update theme favicon etc

--- sites/all/themes/eu4c7/template.php
+++ sites/all/themes/eu4c7/template.php
@@ -4,7 +4,7 @@
 /**
  * Implements hook_preprocess_html()
  */
-function featurebuilder2016_preprocess_html(&$variables) {
+function eu4c7_preprocess_html(&$variables) {
 
   $alias = drupal_get_path_alias (check_plain($_GET ['q']));
   $term = menu_get_object('taxonomy_term', 2);
@@ -20,8 +20,8 @@ function featurebuilder2016_preprocess_html(&$variables) {
     } else {
       $term_path = substr($term_href, 1);
     }
-    // Add path class to body ie .path-support-autocad
 
+    // Add path class to body ie .path-support
     $term_array = explode('/', $term_path);
     $path = $term_array;
     $path = join("/", $path);
@@ -29,38 +29,21 @@ function featurebuilder2016_preprocess_html(&$variables) {
 
     // Add section class to body ie .section-support
     $term_array = explode('/', $term_path);
-    // remove language prefix from path
     $section = $term_array[0];
     $variables['classes_array'][] = drupal_html_class('section-' . $section);
   } else {
-    // Add path class to body ie .path-support-autocad
+    // Add path class to body ie .path-support
     $path = $alias;
     $variables ['classes_array'] [] = 'path-' . str_replace('/', '-', $path);
-
     // Add section class to body ie .section-support
     $term_array = explode('/', $alias);
-    // remove language prefix from path
     $section = $term_array[0];
     $variables['classes_array'][] = drupal_html_class('section-' . $section);
   }
 
-  //  drupal_add_js("sites/all/themes/featurebuilder2016/js/vendor/loglevel.min.js", array('type' => 'file', 'scope' => 'header', 'group' => -100));
-  //  drupal_add_js("sites/all/themes/featurebuilder2016/js/vendor/foundation.min.js", array('type' => 'file', 'scope' => 'header', 'group' => -100));
-  //  drupal_add_js("sites/all/themes/featurebuilder2016/js/vendor/js-cookie.min.js", array('type' => 'file', 'scope' => 'header', 'group' => -100));
-  //  drupal_add_js("sites/all/themes/featurebuilder2016/js/vendor/angular/1.4.4/angular.min.js", array('type' => 'file', 'scope' => 'header', 'group' => -100));
-  //  drupal_add_js("sites/all/themes/featurebuilder2016/js/vendor/angular/1.4.4/angular-sanitize.min.js", array('type' => 'file', 'scope' => 'header', 'group' => -100));
-  //  drupal_add_js("sites/all/themes/featurebuilder2016/js/vendor/angular/1.4.4/angular-route.min.js", array('type' => 'file', 'scope' => 'header', 'group' => -100));
-  //
-  //  drupal_add_js(path_to_theme() . "/js/app/mobilecheck.js", array('type' => 'file', 'scope' => 'footer', 'every_page' => TRUE));
-  //  drupal_add_js(path_to_theme() . "/js/app/akn_header_search.app.js", array('type' => 'file', 'scope' => 'footer', 'every_page' => TRUE));
-  //  drupal_add_js("sites/all/modules/custom/autodesk_faceted_search/js/autodesk_faceted_search.validate.js", array('type' => 'file', 'scope' => 'footer', 'every_page' => TRUE));
-  //
-  //  drupal_add_library('system', 'ui');
-  //  drupal_add_library('system', 'ui.widget');
-
 }
 
-function featurebuilder2016_preprocess_region(&$vars) {
+function eu4c7_preprocess_region(&$vars) {
   $block_number = count($vars['elements']) - 5;
   $vars['classes_array'][''] = 'block-count-' . $block_number;
 }
@@ -69,7 +52,7 @@ function featurebuilder2016_preprocess_region(&$vars) {
 /**
  * Override or insert variables into the page template.
  */
-function featurebuilder2016_process_page(&$variables) {
+function eu4c7_process_page(&$variables) {
   // Convenience variables
   if (!empty($variables['page']['sidebar_first'])){
     $left = $variables['page']['sidebar_first'];
@@ -103,42 +86,17 @@ function featurebuilder2016_process_page(&$variables) {
 /**
  * Implements hook_preprocess_block()
  */
-function featurebuilder2016_preprocess_block(&$variables) {
+function eu4c7_preprocess_block(&$variables) {
 
   // Add a unique class for each block for styling.
   $variables['classes_array'][] = $variables['block_html_id'];
-  // Add classes based on region.
-  switch ($variables['elements']['#block']->region) {
-    // Add a striping class
-    case 'sidebar_first':
-    case 'sidebar_second':
-      $variables['classes_array'][] = 'block-' . $variables['zebra'];
-      switch ($variables['block_html_id']) {
-        case 'block-adsk-translation-translation-block':
-        case 'block-adsk-related-links-adsk-related-links-related':
-        case 'block-block-18':
-          $variables['classes_array'][] = 'clearfix';
-          break;
-      }
-      break;
 
-    case 'header':
-      $variables['classes_array'][] = 'header';
-      break;
-
-    default;
-  }
-
-  if ($variables['block']->module == 'menu') {
-    array_unshift($variables['theme_hook_suggestions'],'block__menu__' . str_replace('-','_',$variables['block']->region) . '__' . str_replace('-','_', $variables['block']->delta));
-    array_unshift($variables['theme_hook_suggestions'],'block__menu__' . str_replace('-','_',$variables['block']->region));
-  }
 }
 
 /**
  * Show contextual links on all nodes
  */
-function featurebuilder2016_node_view_alter( &$build ) {
+function eu4c7_node_view_alter( &$build ) {
   $build['#contextual_links']['node'] = array('node', array($build['#node']->nid));
 }
 
@@ -146,7 +104,7 @@ function featurebuilder2016_node_view_alter( &$build ) {
 /**
  * Implements template_preprocess_node
  */
-function featurebuilder2016_preprocess_node(&$variables) {
+function eu4c7_preprocess_node(&$variables) {
   // Add node--node_type--view_mode.tpl.php suggestions
   $variables['theme_hook_suggestions'][] = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
 
